855c11c6546eb876d4030a66042b593c81407cb0
Jesse Dlouhy <dlouhyjesse@gmail.com>
2020-01-29 11:34:23 -0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 using std::cin;
 using std::cout;
-
+using std::endl;
 int main()
 {
-    cout<<"Hi, please enter two numbers: ";
-
-    return 0;
+	cout<<"Hi, please enter two numbers: " << endl;
+	int intone, inttwo;
+	cin >> intone;
+	cin >> inttwo;
+	return 0;
 }
